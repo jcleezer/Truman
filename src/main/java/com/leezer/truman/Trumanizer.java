9f7ea006b16fa9f04c02bd80a0951031d4e5fd68
Jason Leezer <jleezer@paypal.com>
2018-10-05 13:45:53 -0500

    Add persisting/reading of image index (indexing takes a long time)

--- src/main/java/com/leezer/truman/Trumanizer.java
+++ src/main/java/com/leezer/truman/Trumanizer.java
@@ -1,15 +1,22 @@
 package com.leezer.truman;
 
+import com.google.gson.Gson;
 import com.harium.storage.kdtree.KDTree;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Trumanizer {
 
@@ -17,6 +24,7 @@ public class Trumanizer {
     private final KDTree<File> images;
     private final int gridWidth;
     private final int gridHeight;
+    private final Gson gson;
 
 
     public Trumanizer(File baseImage, File imageDir, int width, int height) throws IOException {
@@ -24,6 +32,7 @@ public class Trumanizer {
         this.images = buildImageIndex(imageDir);
         this.gridHeight = height;
         this.gridWidth = width;
+        this.gson = new Gson();
     }
 
     public BufferedImage trumanize() throws IOException {
@@ -38,9 +47,9 @@ public class Trumanizer {
         for (int x = 0; x < gridWidth; x++){
             for (int y = 0; y < gridHeight; y++){
                 BufferedImage subImage = base.getSubimage(x*tileWidth,y*tileHeight,tileWidth,tileHeight);
-                double[] rgb = getRGB(subImage);
+                RGB rgb = getRGB(subImage);
                 //TODO we need to shrink this image
-                final File nearestNeighbor = images.nearest(rgb);
+                final File nearestNeighbor = images.nearest(new double[]{rgb.getRed(),rgb.getGreen(),rgb.getBlue()});
                 final BufferedImage shrunkImage = resizeImage(ImageIO.read(nearestNeighbor),tileWidth,tileHeight);
                 // Write image to output
                 for (int w = 0; w < shrunkImage.getWidth(); w++){
@@ -65,33 +74,47 @@ public class Trumanizer {
 
     private KDTree<File> buildImageIndex(File imageDir) throws IOException {
         final KDTree<File> index = new KDTree<>(3);
+        Map<String,RGB> values = new HashMap<>();
+        final File persisted = new File(imageDir,"truman.json");
+        if (persisted.exists()){
+            Map<String,RGB> persistedRgbs = gson.fromJson(new FileReader(persisted),Map.class);
+            values.putAll(persistedRgbs);
+        }
+
         Files.walk(Paths.get(imageDir.getAbsolutePath()))
                 .filter(Files::isRegularFile)
                 .filter(f -> f.endsWith("jpg"))
-                .forEach( path -> addToTree(index,path));
+                .forEach( path -> addToMap(values,path));
+
+        //Persist
+        IOUtils.write(gson.toJson(values),new FileWriter(persisted));
+        for (Map.Entry<String,RGB> entires : values.entrySet()){
+            RGB rgb = entires.getValue();
+            index.insert(new double[]{rgb.getRed(),rgb.getGreen(),rgb.getBlue()},new File(entires.getKey()));
+        }
         return index;
     }
 
-    private void addToTree(KDTree<File> index, Path path)  {
+    private void addToMap(Map<String,RGB> values, Path path)  {
         final File imageFile = path.toFile();
 
         try {
             BufferedImage image = ImageIO.read(imageFile);
-            double[] rgb = getRGB(image);
+            RGB rgb = getRGB(image);
 
             System.out.println("Processing "+path.toFile().getName());
-            System.out.println("Red Color value = "+ rgb[0]);
-            System.out.println("Green Color value = "+ rgb[1]);
-            System.out.println("Blue Color value = "+ rgb[2]);
+            System.out.println("Red Color value = "+ rgb.getRed());
+            System.out.println("Green Color value = "+ rgb.getGreen());
+            System.out.println("Blue Color value = "+ rgb.getBlue());
 
-            index.insert(rgb,imageFile);
+            values.put(path.toString(),rgb);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public double[] getRGB(BufferedImage image){
+    public RGB getRGB(BufferedImage image){
         int count = 0;
 
         int redTotal = 0;
@@ -115,6 +138,6 @@ public class Trumanizer {
         double blue = blueTotal/count;
         double red = redTotal/count;
         double green = greenTotal/count;
-        return new double[]{red,green,blue};
+        return new RGB(red,green,blue);
     }
 }
